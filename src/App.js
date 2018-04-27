76a1182354dd15eb82bbf960c624ace31b486b14
Azat Temirbek uulu <e178551@metu.edu.tr>
2018-04-28 00:57:22 +0300

    Base class created

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import ALERTS from './alerts.json';
-import NOTES from './notes.json';
+import ALERTS from './data/alerts.json';
+import NOTES from './data/notes.json';
 
-import './App.css';
-class App extends Component {
-    constructor() {
-        super();
+/**
+ * base class to easy connection between components
+ * i will extend all the components from this Base Component
+ * todo export class to another file
+ */
+export class Base extends Component{
+    constructor(props){
+        super(props)
         this.state = {
             alerts: ALERTS.alerts,
             range: ALERTS.dateTimeRange,
             notes: NOTES.notes
         };
     }
+}
+
+/**
+ * Start ui class
+ */
+import './App.css';
+class App extends Base {
+    constructor() {
+        super();
+    }
 
     render() {
+        console.log(this.state);
         return <div className="DottedBox">data</div>
     }
 }
